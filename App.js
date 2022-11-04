cac9b7a7babbb8628549900e020364b98dfe114e
Tharindu Samarakoon <tharusamara@gmail.com>
2022-11-05 04:07:59 +0530

    Small changes

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import FinancialInstitutions from './componenets/FinancialInstitutions';
 import LoanListView from './componenets/LoanListView';
 import UserLoanLeaseRequests from './componenets/UserLoanLeaseRequests';
 
