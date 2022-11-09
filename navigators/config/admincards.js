106b6fc5ada8c2a88ecddd880c16adc40e7cd20e
blackxtormxx <it20061166@my.sliit.lk>
2022-11-10 02:56:08 +0530

    Advisor card updared

--- navigators/config/admincards.js
+++ navigators/config/admincards.js
@@ -2,50 +2,50 @@
 export default [
     {
       id: 1,
-      name: "Loan Request",
-      image: require("../../assets/caards/loan-application-pending-icon.png"),
+      name: "Requested loan",
+      image: require("../../assets/caards/qwa.jpg"),
       url: 'Welcome'
       
     },
     {
       id: 2,
-      name: "Leasing Request",
-      image: require("../../assets/caards/car-price-icon.png"),
+      name: "Requested lease",
+      image: require("../../assets/caards/istockphoto-612027090-1024x1024.jpg"),
       url: 'Welcome'
       
     },
     {
       id: 3,
       name: "Financial Institutions ",
-      image: require("../../assets/caards/financial-statement-icon.png"),
+      image: require("../../assets/caards/1668025816654.jpg"),
       url: 'Welcome'
       
     },
     {
       id: 4,
       name: "View Inquiries",
-      image: require("../../assets/caards/strategy-icon.png"),
+      image: require("../../assets/caards/1668026046945.jpg"),
       url: 'Welcome'
       
     },
     {
       id: 5,
       name: "Requested Advisers",
-      image: require("../../assets/caards/query-what-how-why-icon.png"),
+      image: require("../../assets/caards/1668026265138.jpg"),
       url: 'AdvisorsRequests'
 
     },
     {
       id: 6,
       name: "Inspection Form",
-      image: require("../../assets/caards/edit-list-icon.png"),
+      image: require("../../assets/caards/er3.jpg"),
       url: 'Welcome'
 
     },
     {
       id: 7,
       name: "Inspection List",
-      image: require("../../assets/caards/check-list-icon.png"),
+      image: require("../../assets/caards/1668026411513.jpg"),
       url: 'Welcome'
 
     },
